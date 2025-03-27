2f9107b92fc3aae8620883e986b1906d8b7a9522
prakul19 <prakulagarwal5@gmail.com>
2025-03-27 13:49:24 +0530

    [prakul] stats: created an abstract generic class of Product

---
+++ src/com/bridgelabz/onlinemarketplace/Product.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.onlinemarketplace;
+
+public abstract class Product<T> {
+    private String name;
+    private double price;
+    private T category;
+
+    public Product(String name, double price, T category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public T getCategory() {
+        return category;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public abstract void displayInfo();
+}
