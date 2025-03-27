c9491083240d8a6165ad507b35705bafca0404ff
prakul19 <prakulagarwal5@gmail.com>
2025-03-27 13:24:47 +0530

    [prakul] stats:created an abstract class WarehouseItem

---
+++ src/com/bridgelabz/smartwarehouse/WarehouseItem.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.smartwarehouse;
+
+public abstract class WarehouseItem {
+    private String name;
+    private double price;
+
+    public WarehouseItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract void displayInfo();
+}
