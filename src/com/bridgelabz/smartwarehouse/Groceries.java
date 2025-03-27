3e4e1d8cd33cd7b2a6c1762c893d6a89ff5ad31e
prakul19 <prakulagarwal5@gmail.com>
2025-03-27 13:27:49 +0530

    [prakul] stats: created subclass Groceries that exrends WarehouseItem

---
+++ src/com/bridgelabz/smartwarehouse/Groceries.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.smartwarehouse;
+
+public class Groceries extends WarehouseItem {
+    private String expirationDate;
+
+    public Groceries(String name, double price, String expirationDate) {
+        super(name, price);
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Grocery: " + getName() + " | Expiry: " + expirationDate + " | Price: $" + getPrice());
+    }
+}
